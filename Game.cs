eb5d6996d127701737d84233b9ac1447cc4b2dd3
Madjam05 <162447804+Madjam05@users.noreply.github.com>
2024-05-04 23:35:50 +0100

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -8,6 +8,7 @@ internal class Game
     int total;
     int rtotal1;
     int rtotal2;
+    bool playing = True
 
 
     Die dice1 - new Die();
@@ -18,7 +19,7 @@ internal class Game
 
     public void  SevensOut()
     {
-        while true
+        while playing = true
         {
             valued1 = dice1.dice_roll;
             valued2 = dice2.dice_roll;
@@ -32,7 +33,7 @@ internal class Game
             {
                 total = total*2;
             }
-            rtotal1 = +rtotal1+total;
+            rtotal1 = rtotal1+total;
         }
         return rtotal1
     }
