d21fbe67bd584dc542d05cd73c0ec7b9cf8177a3
Madjam05 <162447804+Madjam05@users.noreply.github.com>
2024-05-04 16:29:02 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+Game new_game = new Game();
+Testing test = new Testing();
+
+Testing.test();
+
+string choice;
+while true
+{
+    new_game.SevensOut();
+    console.WriteLine("Your Score for this game is" + new_game.rtotal1 + "Would you like to play again?");
+    choice = console.readline
+    if(choice == "no") or (choice == "No") or (choice == "NO")
+    {
+        break;
+    }
+}
